c8d5f421c131dff3dfa1743a5504397a0a3bea46
gcaze <gcaze@monsanto.com>
2019-06-03 15:53:51 -0300

    Added new files WIP

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default() => (
+    <div style={{ color: 'tea'}}>
+        <h1>About gatsby</h1>
+        <p>We are the best</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import Header from '../components/header';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+export default () => (
+        <div style={{color: 'purple'}}>
+            <Link to='/contact'>Contact</Link>
+            <Header headerText='Index' />
+            <h1>Hello world!</h1>
+            <p>What a world.</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+)
+
+
